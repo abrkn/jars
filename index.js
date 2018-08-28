f724b9d88f4b1961c00d94ba3386906f0bbf44ae
Andreas Brekken <andreas@brekken.com>
2018-08-28 17:59:21 +0700

    Fix exports

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-module.exports = {
+Object.assign(exports, {
   createClient: require('./client'),
   createServer: require('./server'),
   createRouter: require('./router'),
   createApplication: require('./application'),
   createApp: require('./application'),
-};
+});
